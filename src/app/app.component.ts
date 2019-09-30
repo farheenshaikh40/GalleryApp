a99fa16cec7d321c95f25a4014c6105e2c36ec84
LAPTOP-TH0C7U1P\Fareen Shaikh <farheenshaikh40>
2019-10-01 00:37:47 +0530

    images added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'gallery';
 }
+
+// const IMAGES = [
+//   {id:1, category:'cats', caption:'Cat No. 1', url:'../assets/images/c1.jpg'},
+//   {id:2, category:'cats', caption:'Cat No. 2', url:'../assets/images/c2.jpg'},
+//   {id:3, category:'cats', caption:'Cat No. 3', url:'../assets/images/c3.jpg'},
+//   {id:4, category:'cats', caption:'Cat No. 4', url:'../assets/images/c4.jpg'},
+//   {id:5, category:'cats', caption:'Cat No. 5', url:'../assets/images/c5.jpg'},
+//   {id:6, category:'cats', caption:'Cat No. 6', url:'../assets/images/c6.jpg'},
+//   {id:7, category:'dogs', caption:'Dog No. 1', url:'../assets/images/d1.jpg'},
+//   {id:8, category:'dogs', caption:'Dog No. 1', url:'../assets/images/d2.jpg'},
+//   {id:9, category:'dogs', caption:'Dog No. 1', url:'../assets/images/d3.jpg'},
+//   {id:10, category:'hamster', caption:'hamster No. 1', url:'../assets/images/h1.jpg'}
+// ]
